3aa0c360864e7324b1eb7b4a5074c51034b61378
Eduard <mr.herocaner@gmail.com>
2024-04-27 12:45:14 +0300

    Задача 3

---
+++ Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая будет принимать на
+// вход два числа и выводить, является ли первое число
+// кратным второму. Если первое число некратно
+// второму, то программа выводит остаток от деления.
+// Примеры
+// 14, 5 => нет, 4
+// 16, 8 => да
+// 4, 3 => нет, 1
+
+Console.WriteLine("Input first number:");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input second number:");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int remainder = firstNumber % secondNumber;
+
+if (firstNumber % secondNumber == 0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+   Console.WriteLine("No," + remainder);
+
+}
